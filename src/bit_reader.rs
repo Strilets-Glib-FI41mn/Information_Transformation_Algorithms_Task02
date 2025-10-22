66b051a68a5143cf90cbdc713e2222b38ec7d056
Strilets Glib FI41mn <181478164+Strilets-Glib-FI41mn@users.noreply.github.com>
2025-10-22 19:40:58 +0300

    start cli work

--- src/bit_reader.rs
+++ src/bit_reader.rs
@@ -9,17 +9,23 @@ pub struct FileBitReader{
 impl FileBitReader{
     pub fn read_bits(&mut self, len: usize)  -> std::io::Result<Vec<bool>> {
         let mut result = vec![];
-        let requided_bits = len - (8 - self.buff_index.unwrap_or(8));
-        //let requided_bits = len - self.buff_index.unwrap_or(0);
-        let all_bytes_output = requided_bits % 8 == 0;
-        let required_bytes = (requided_bits) / 8 + {match all_bytes_output {true => 0, false => 1}};
+        if cfg!(feature = "debug_data"){
+            println!("self.buff_index: {:?}", self.buff_index);
+        }
+        let left_in_buffer = 8 - self.buff_index.unwrap_or(8);
+        let required_bits = {match len < left_in_buffer {
+            true => 0,
+            false => len - left_in_buffer
+        }};
+        //let required_bits = len - self.buff_index.unwrap_or(0);
+        let all_bytes_output = required_bits % 8 == 0;
+        let required_bytes = (required_bits) / 8 + {match all_bytes_output {true => 0, false => 1}};
         let mut buffer =vec![0; required_bytes];
 
         if cfg!(feature = "debug_data"){
-            println!("requided_bits: {}", requided_bits);
+            println!("required_bits: {}", required_bits);
             println!("required_bytes: {}", required_bytes);
             println!("all_bytes_output: {}", all_bytes_output);
-            println!("self.buff_index: {:?}", self.buff_index);
         }
 
         self.buf_reader.read_exact(&mut buffer)?;
@@ -45,10 +51,10 @@ impl FileBitReader{
                         result.push((buffer[j] >> 7 - i) & 1 == 1);
                     }
                 }
-                for i in 0..requided_bits % 8{
+                for i in 0..required_bits % 8{
                     result.push((buffer[required_bytes - 1] >> 7 - i) & 1 == 1);
                 }
-                self.buff_index = Some((requided_bits) % 8);
+                self.buff_index = Some((required_bits) % 8);
                 self.buffer = buffer[required_bytes - 1];
 
             },
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,88 @@
-use std::fs::File;
+use std::{fmt::Display, fs::File};
 
 use bit_reader::FileBitReader;
 use bit_writter::FileBitWriter;
 mod bit_writter;
 mod bit_reader;
 use bool_vec::bool_vec;
+use inquire_derive::Selectable;
+//use inquire::{error::InquireError, Select};
+#[derive(PartialEq)]
+enum State{
+    Started,
+    Writting,//(Option<FileBitWriter>),
+    Reading,//(Option<FileBitReader>),
+    Exiting
+}
+#[derive(Debug, Copy, Clone, Selectable)]
+#[allow(clippy::upper_case_acronyms)]
+
+
+enum Action{
+    Read,
+    Write,
+    Exit
+}
+impl Display for Action{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Action::Read => write!(f, "Read file"),
+            Action::Write => write!(f, "Write file"),
+            Action::Exit => write!(f, "Exit"),
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>>  {
-    println!("Hello, world!");
+    let mut file_writter: Option<FileBitWriter> = None;
+    let mut file_reader: Option<FileBitReader> = None;
+    let mut state = State::Started;
+    while state != State::Exiting{
+        let selected = Action::select("").prompt();
+        match selected{
+            Ok(Action::Exit) =>{
+                state = State::Exiting;
+            }
+            Ok(Action::Write) =>{
+                if file_reader.is_some(){
+                    file_reader = None;
+                }
+                if file_writter.is_none(){
+                    let file_name = inquire::Text::new("Into what file write binary data")
+                    .prompt().unwrap();
+                    let file = File::create(&file_name).unwrap();
+                    file_writter = Some(FileBitWriter::new(file));
+                }
+                let data =  inquire::Text::new("Data:").prompt().unwrap();
+                if let Some(writter) = & mut file_writter{
+                    writter.write_bits(bool_vec_from_string(&data));
+                }
+            }
+
+            Ok(Action::Read) =>{
+                if file_writter.is_some(){
+                    file_writter = None;
+                }
+                if file_reader.is_none(){
+                    let file_name = inquire::Text::new("Into what file write binary data")
+                    .prompt().unwrap();
+                    let file = File::open(&file_name).unwrap();
+                    file_reader = Some(FileBitReader::new(file));
+                }
+                if let Some(reader) = &mut file_reader{
+                    let input_string =  inquire::Text::new("How many bits?:").prompt().unwrap();
+                    let len = input_string.parse().expect("Not a valid number");
+                    let result = reader.read_bits(len);
+                    println!("Read value: {:?}", result);
+
+                }
+            }
+            _ => {
+                println!("Not implemented yet")
+            }
+        }
+    }
+
     //compile_error!();
     let file = File::create("foo").unwrap();
     let mut file_writter = FileBitWriter::new(file);
@@ -17,7 +93,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
     file_writter.write_bits(bool_vec!(1111))?;
     let file = File::open("foo").unwrap();
     let mut file_reader = FileBitReader::new(file);
-    println!("{:?}",file_reader.read_bits(8));
+    println!("{:?}",file_reader.read_bits(3));
+    println!("{:?}",file_reader.read_bits(5));
+    //println!("{:?}",file_reader.read_bits(8));
     println!("{:?}",file_reader.read_bits(10));
     println!("{:?}",file_reader.read_bits(6));
     
